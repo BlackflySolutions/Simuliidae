4053a3de78c36ebc5405fc6ed1d876e980659256
Alan Dixon <alan.g.dixon@gmail.com>
2019-04-19 13:33:35 -0400

    admin.sh script improvements

--- 8.6/apache/admin/admin.sh
+++ 8.6/apache/admin/admin.sh
@@ -2,6 +2,7 @@
 # Use my composer to build code if the base directory is empty
 # Use the composer-drupal-optimizations to reduce memory use!
 if [ -z "$(ls -A /var/www/drupal)" ]; then
+  echo "Empty codebase, seting up latest stable Drupal code using composer."
   cd /var/www/drupal
   chown drupal .
   sudo -u drupal composer create-project drupal-composer/drupal-project:8.x-dev . --no-interaction  --no-install
@@ -9,14 +10,18 @@ if [ -z "$(ls -A /var/www/drupal)" ]; then
   sudo -u drupal composer install
   mkdir -p /var/www/drupal/config/sync
   chown www-data:www-data /var/www/drupal/config/sync
+else
+  echo "Detected existing Drupal codebase."
 fi
 cd /var/www/drupal
 # Use drush to do a standard Drupal install, if settings file doesn't have my password
 #if [ ! -f /var/www/drupal/web/sites/default/settings.php ]; then
 if grep -q $MYSQL_PASSWORD /var/www/drupal/web/sites/default/settings.php; then
   # if I have a configured settings file, just run the updatedb and wait for further attention in bash
-  sudo -E -u www-data drush cr
-  sudo -E -u www-data drush updatedb
+  echo "Drupal already installed"
+  echo "Site is ready at https://${VSITE_DOMAIN}"
+  # sudo -E -u www-data drush cr
+  # sudo -E -u www-data drush updatedb
 else
   /usr/local/bin/wait-for-it.sh vsql:3306
   # this will destroy any existing db, should I check first?
@@ -49,8 +54,8 @@ else
   sudo -E -u www-data drush -y config-set system.theme admin seven
   sudo -E -u www-data drush -y config-set system.theme default ${VSITE_THEME:-seven}
   echo "Site Installation Completed"
-  # TODO: report back to root that I have completed!
+  echo "Login using the following url"
+  sudo -E -u www-data drush --uri="https://${VSITE_DOMAIN}" uli
 fi
-echo "Site is ready at https://${VSITE_DOMAIN}"
-echo "Login using the following url"
-sudo -E -u www-data drush uli
+#echo "Login using the following url"
+#sudo -E -u www-data drush uli
