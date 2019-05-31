d3c66c028fd08efbb964b57007ed112fd2fdb382
Alan Dixon <alan.g.dixon@gmail.com>
2019-05-31 10:47:50 -0400

    new and improved stack-generate, including multiple adds, and variant-specific adds

--- stack-generate.sh
+++ stack-generate.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 # 1. project variant (directory)
 # 2. compose file additions
+export THIS_NODE=`hostname`
+echo "Using $THIS_NODE"
 export VSITE_COMPOSE_PROJECT_VARIANT=$1
-BASE=${3:-base}.yml
-if [[ -z $3 ]]; then
-  GENNAME=$2.yml
-else
-  GENNAME=${3}-$2.yml
-fi
-docker-compose -f ${VSITE_COMPOSE_PROJECT_VARIANT}/$BASE -f ${2}.yml config | sed 's/{VSITE/\${VSITE/g' | sed "s/ \([^[:space:]]*\): ''$/ - \1/" > ${VSITE_COMPOSE_PROJECT_VARIANT}/$GENNAME
+shift
+BASE="base"
+GENNAME=""
+CONFIGS=""
+# iterate
+while test ${#} -gt 0
+do
+  ADD=$1
+  if [[ -z $GENNAME ]]; then
+    GENNAME=${ADD}
+  else
+    GENNAME=${GENNAME}-${ADD}
+  fi
+  if [[ -f "${VSITE_COMPOSE_PROJECT_VARIANT}/-${ADD}.yml" ]]; then
+    CONFIGS="$CONFIGS -f ${VSITE_COMPOSE_PROJECT_VARIANT}/-${ADD}.yml"
+  else
+    CONFIGS="$CONFIGS -f $ADD.yml"
+  fi
+  shift
+done
+docker-compose -f ${VSITE_COMPOSE_PROJECT_VARIANT}/${BASE}.yml $CONFIGS config | sed 's/{VSITE/\${VSITE/g' | sed "s/ \([^[:space:]]*\): ''$/ - \1/" > ${VSITE_COMPOSE_PROJECT_VARIANT}/${GENNAME}.yml
