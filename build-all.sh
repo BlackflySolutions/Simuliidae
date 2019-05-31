32ef3c26b4e5e4a6b1e50db239dfde7279ab5111
Alan Dixon <alan.g.dixon@gmail.com>
2019-05-31 12:00:06 -0400

    updated build script for civicrm-5 branch

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker build 6/apache/admin -t simuliidae-admin:6-apache
-docker build 7/apache/admin -t simuliidae-admin:7-apache
-docker build 8/apache/admin -t simuliidae-admin:8-apache
+docker build 6/apache/admin -t simuliidae-admin:6-apache-civicrm-5
+docker build 7/apache/admin -t simuliidae-admin:7-apache-civicrm-5
+docker build 8/apache/admin -t simuliidae-admin:8-apache-civicrm-5
