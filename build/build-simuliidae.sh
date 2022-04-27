6bd4999d29344ed691f6e6a28c1e64cb2d513ab2
Alan Dixon <alan.g.dixon@gmail.com>
2022-04-27 14:22:18 -0400

    updates to convenience scripts

--- build/build-simuliidae.sh
+++ build/build-simuliidae.sh
@@ -4,3 +4,5 @@
 # REPOSITORY_FROM='blackflysolutions/';
 IMAGE_FROM=drupal-civicrm-base:7-php7.4-apache-buster DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM --target=vhttp -t simuliidae-base-vhttp:7-php7.4-apache-buster 7/php7.4/apache-buster/
 IMAGE_FROM=drupal-civicrm-base:7-php7.4-apache-buster DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM --target=admin -t simuliidae-base-admin:7-php7.4-apache-buster 7/php7.4/apache-buster/
+IMAGE_FROM=drupal-civicrm-base:9.3-php7.4-apache-buster DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM --target=vhttp -t simuliidae-base-vhttp:9.3-php7.4-apache-buster 9.3/php7.4/apache-buster/
+IMAGE_FROM=drupal-civicrm-base:9.3-php7.4-apache-buster DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM --target=admin -t simuliidae-base-admin:9.3-php7.4-apache-buster 9.3/php7.4/apache-buster/
---
+++ build/push-simuliidae.sh
@@ -0,0 +1,6 @@
+# tag all the simuliidae images
+# REPOSITORY_FROM='blackflysolutions/';
+docker push blackflysolutions/simuliidae-base-vhttp:7-php7.4-apache-buster 
+docker push blackflysolutions/simuliidae-base-admin:7-php7.4-apache-buster 
+docker push blackflysolutions/simuliidae-base-vhttp:9.3-php7.4-apache-buster 
+docker push blackflysolutions/simuliidae-base-admin:9.3-php7.4-apache-buster 
