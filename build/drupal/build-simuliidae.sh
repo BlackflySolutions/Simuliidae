c273fce5b08ec099b443bc50c821b450640599bd
Alan Dixon <alan.g.dixon@gmail.com>
2022-09-21 17:26:27 -0400

    working!

--- build/drupal/build-simuliidae.sh
+++ build/drupal/build-simuliidae.sh
@@ -7,8 +7,8 @@
 while read VARIANT_DIR; do
   echo "$VARIANT_DIR"
   VARIANT_TAG=$(echo $VARIANT_DIR | sed -e 's/\//-/g')
-  IMAGE_FROM=drupal:base-$VARIANT_TAG DOCKER_BUILDKIT=1 docker build -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae:vhttp-base-$VARIANT_TAG $VARIANT_DIR
-  IMAGE_FROM=drupal:base-$VARIANT_TAG DOCKER_BUILDKIT=1 docker build -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae:admin-base-$VARIANT_TAG $VARIANT_DIR
-  IMAGE_FROM=drupal:drupal-$VARIANT_TAG DOCKER_BUILDKIT=1 docker build -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae:vhttp-drupal-$VARIANT_TAG $VARIANT_DIR
-  IMAGE_FROM=drupal:drupal-$VARIANT_TAG DOCKER_BUILDKIT=1 docker build -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae:admin-drupal-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:base-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae:vhttp-base-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:base-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae:admin-base-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:drupal-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae:vhttp-drupal-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:drupal-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae:admin-drupal-$VARIANT_TAG $VARIANT_DIR
 done <variants.txt
