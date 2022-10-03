5e179563d95b27b187ae80f254f1dab626f873ff
Alan Dixon <alan.g.dixon@gmail.com>
2022-10-03 20:04:54 +0000

    moved shell scripts into bin dir

--- compose/build-all.sh
+++
@@ -1,5 +0,0 @@
-# build all my images
-# REPOSITORY_FROM='blackflysolutions/';
-# docker build 6/apache/admin -t simuliidae-admin:6-apache
-docker build --build-arg REPOSITORY_FROM 7/apache/admin -t simuliidae-admin:7-apache
-docker build --build-arg REPOSITORY_FROM 8/apache/admin -t simuliidae-admin:8-apache
