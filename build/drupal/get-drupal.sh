053ced0b019e6d5dee4ec2d096579c016ce72b4d
Alan Dixon <alan.g.dixon@gmail.com>
2022-10-03 21:22:43 +0000

    split off part of build-drupal into get-drupal

--- build/drupal/get-drupal.sh
+++ build/drupal/get-drupal.sh
@@ -6,7 +6,5 @@
 # docker build 6/apache/admin -t simuliidae-admin:6-apache
 while read VARIANT_DIR; do
   echo "$VARIANT_DIR"
-  VARIANT_TAG=$(echo $VARIANT_DIR | sed -e 's/\//-/g')
-  DOCKER_BUILDKIT=1 docker build --target=drupal-base -t drupal:base-$VARIANT_TAG $VARIANT_DIR
-  DOCKER_BUILDKIT=1 docker build --target= -t drupal:drupal-$VARIANT_TAG $VARIANT_DIR
+  cp ../../reference/drupal/$VARIANT_DIR/Dockerfile $VARIANT_DIR/
 done <variants.txt
