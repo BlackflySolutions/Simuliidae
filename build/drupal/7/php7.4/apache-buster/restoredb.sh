29115885364e327b9d8616eab76d5f8a5019a2a0
Alan Dixon <alan.g.dixon@gmail.com>
2022-10-11 20:41:42 +0000

    improved restoredb function

--- build/drupal/7/php7.4/apache-buster/restoredb.sh
+++ build/drupal/7/php7.4/apache-buster/restoredb.sh
@@ -5,28 +5,25 @@ MYSQL_CMD=/usr/bin/mysql
 #
 DB_USER="root"
 DB_PASSWORD="$MYSQL_ROOT_PASSWORD"
-DB_NAMES="$MYSQL_DATABASE $CIVICRM_DATABASE"
+# DB_NAMES="$MYSQL_DATABASE $CIVICRM_DATABASE"
 DB_HOST="vsql"
 #
 RESTORE_DIR=/var/restore/vsql
+DB_NAMES=`ls /var/restore/vsql/*sql`
 #
 # set $(date)
 #
 # restore a database from it's origin backup
 # echo "$MYSQLIMPORT_CMD --local -u $DB_USER --delete $DB_NAME $RESTORE_DIR/*txt"
-for DB_NAME in $DB_NAMES
+for DB_FILENAME in $DB_NAMES
 do
-  if [ $DB_NAME != "Database" -a $DB_NAME != "test" -a $DB_NAME != "mysql" ] 
-  then 
-    if [ -f $RESTORE_DIR/${DB_NAME}.sql ]
-    then
-      echo $RESTORE_DIR/$DB_NAME
-      start=`date +%s`
-      $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e"DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME; GRANT ALL ON $DB_NAME.* to $MYSQL_USER@'%'; ";
-      $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME < $RESTORE_DIR/${DB_NAME}.sql
-      end=`date +%s`
-      runtime=$((end-start))
-      echo "runtime: $runtime"
-    fi
-  fi 
+  DB_FID=$(basename -- "$DB_FILENAME")
+  DB_NAME="${DB_FID%.*}"
+  echo $RESTORE_DIR/$DB_NAME
+  start=`date +%s`
+  $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e"DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME; GRANT ALL ON $DB_NAME.* to $MYSQL_USER@'%'; ";
+  $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME < $RESTORE_DIR/${DB_NAME}.sql
+  end=`date +%s`
+  runtime=$((end-start))
+  echo "runtime: $runtime"
 done
