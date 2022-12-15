df3874de77ffd6cb1a65a42671cef64c241946d7
Alan Dixon <alan.g.dixon@gmail.com>
2022-12-15 16:59:39 -0500

    php8.0 variant for drupal 7

---
+++ build/drupal/7/php8.0/apache-buster/restoredb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# restores backups 
+MYSQLIMPORT_CMD=/usr/bin/mysqlimport
+MYSQL_CMD=/usr/bin/mysql
+#
+DB_USER="root"
+DB_PASSWORD="$MYSQL_ROOT_PASSWORD"
+# DB_NAMES="$MYSQL_DATABASE $CIVICRM_DATABASE"
+DB_HOST="vsql"
+#
+RESTORE_DIR=/var/restore/vsql
+DB_NAMES=`ls /var/restore/vsql/*sql`
+#
+# set $(date)
+#
+# restore a database from it's origin backup
+# echo "$MYSQLIMPORT_CMD --local -u $DB_USER --delete $DB_NAME $RESTORE_DIR/*txt"
+for DB_FILENAME in $DB_NAMES
+do
+  DB_FID=$(basename -- "$DB_FILENAME")
+  DB_NAME="${DB_FID%.*}"
+  echo $RESTORE_DIR/$DB_NAME
+  start=`date +%s`
+  $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e"DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME; GRANT ALL ON $DB_NAME.* to $MYSQL_USER@'%'; ";
+  $MYSQL_CMD -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME < $RESTORE_DIR/${DB_NAME}.sql
+  end=`date +%s`
+  runtime=$((end-start))
+  echo "runtime: $runtime"
+done
