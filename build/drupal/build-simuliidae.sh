04dc2eb521040229f080af266a5a1ca4feb181af
Alan Dixon <alan.g.dixon@gmail.com>
2023-02-10 16:08:27 -0500

    custom entrypoint.sh + use VARIANT_TAG instead of IMAGE_FROM argument in build for more flexibility + use of different build stages for base and drupal variants

--- build/drupal/build-simuliidae.sh
+++ build/drupal/build-simuliidae.sh
@@ -2,13 +2,13 @@
 # includes the 'base' images that do NOT include the drupal code
 # the civicrm in the image name refers to the additions in the required stack (see the modifications to Dockerfile.template)
 # i.e. none of these include civicrm code
-# REPOSITORY_FROM='blackflysolutions/';
+REPOSITORY_FROM='blackflysolutions/';
 # docker build 6/apache/admin -t simuliidae-admin:6-apache
 while read VARIANT_DIR; do
   echo "$VARIANT_DIR"
   VARIANT_TAG=$(echo $VARIANT_DIR | sed -e 's/\//-/g')
-  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:base-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae-drupal:vhttp-base-$VARIANT_TAG $VARIANT_DIR
-  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:base-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae-drupal:admin-base-$VARIANT_TAG $VARIANT_DIR
-  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:drupal-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp -t simuliidae-drupal:vhttp-drupal-$VARIANT_TAG $VARIANT_DIR
-  DOCKER_BUILDKIT=1 docker build --build-arg IMAGE_FROM=drupal:drupal-$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin -t simuliidae-drupal:admin-drupal-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg REPOSITORY_FROM=$REPOSITORY_FROM --build-arg VARIANT_TAG=$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp-base -t simuliidae-drupal:vhttp-base-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg REPOSITORY_FROM=$REPOSITORY_FROM --build-arg VARIANT_TAG=$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin-base -t simuliidae-drupal:admin-base-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg REPOSITORY_FROM=$REPOSITORY_FROM --build-arg VARIANT_TAG=$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=vhttp-drupal -t simuliidae-drupal:vhttp-drupal-$VARIANT_TAG $VARIANT_DIR
+  DOCKER_BUILDKIT=1 docker build --build-arg REPOSITORY_FROM=$REPOSITORY_FROM --build-arg VARIANT_TAG=$VARIANT_TAG -f $VARIANT_DIR/Simuliidae --target=admin-drupal -t simuliidae-drupal:admin-drupal-$VARIANT_TAG $VARIANT_DIR
 done <variants.txt
