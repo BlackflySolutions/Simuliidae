9caf2a81bf9eb76f9f4c3cccd5e76e4222ee7461
Alan Dixon <alan.g.dixon@gmail.com>
2023-03-27 12:54:20 -0400

    removed cp to separate script + only build base

--- build/wordpress/build-wordpress.sh
+++ build/wordpress/build-wordpress.sh
@@ -4,7 +4,6 @@
 while read VARIANT_DIR; do
   echo "$VARIANT_DIR"
   VARIANT_TAG=$(echo $VARIANT_DIR | sed -e 's/\//-/g')
-  cp ../../reference/wordpress/$VARIANT_DIR/* $VARIANT_DIR/
   DOCKER_BUILDKIT=1 docker build --target=wordpress-base -t wordpress:base-$VARIANT_TAG $VARIANT_DIR
-  DOCKER_BUILDKIT=1 docker build --target= -t wordpress:wordpress-$VARIANT_TAG $VARIANT_DIR
+  #DOCKER_BUILDKIT=1 docker build --target= -t wordpress:wordpress-$VARIANT_TAG $VARIANT_DIR
 done <variants.txt
