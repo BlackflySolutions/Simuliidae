c741dba7a1b25a8e2d1775a0b92bc22155722285
Alan Dixon <alan.g.dixon@gmail.com>
2023-09-07 10:38:55 -0400

    removed 9.3, deprecated

--- build/drupal/9.3/php7.4/apache-buster/admin/get-l10n.sh
+++
@@ -1,3 +0,0 @@
-sudo -u drupal curl -o /tmp/civicrm-l10n.tar.gz -L -O https://download.civicrm.org/latest/civicrm-${VSITE_CIVICRM_VER}-l10n.tar.gz 
-sudo -u drupal tar -xzf /tmp/civicrm-l10n.tar.gz --strip-components=1 -C /var/www/drupal/vendor/civicrm/civicrm-core
-rm /tmp/civicrm-l10n.tar.gz
--- build/drupal/9.3/php7.4/apache-buster/entrypoint-sshd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/usr/local/bin/initialize.sh
-if [[ ! -z "$VSITE_SSH_USER" ]]; then
-  mkdir /root/.ssh/
-  cp /home/${VSITE_SSH_USER}/.ssh/hosted_id_rsa.pub /root/.ssh/authorized_keys
-fi
-exec /usr/sbin/sshd -D
