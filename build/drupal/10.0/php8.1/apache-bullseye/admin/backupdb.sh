2cf383725bfea6aa730de885f5f43e477950dcab
Alan Dixon <alan.g.dixon@gmail.com>
2024-01-23 22:44:02 -0500

    removed deprecated Drupal 10.0

--- build/drupal/10.0/php8.1/apache-bullseye/admin/backupdb.sh
+++
@@ -1,4 +0,0 @@
-# use drush to generate ad-hoc backups
-TODAY=$(date +"%Y-%m-%d")
-drush -y sql-dump --ordered-dump --gzip --result-file=/var/backup/vsql/drupal-${TODAY}.sql
-drush -y civicrm-sql-dump --gzip --result-file=/var/backup/vsql/civicrm-${TODAY}.sql
--- build/drupal/10.0/php8.2/apache-bullseye/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/usr/local/bin/initialize.sh
-# Hand off to the CMD
-if test -z "$@"
-then
-  apache2-foreground
-else
-  exec "$@"
-fi
