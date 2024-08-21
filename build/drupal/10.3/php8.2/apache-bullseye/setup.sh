3d4c9b06a2543b086de7bb0169cfff85a23c2910
Alan Dixon <alan.g.dixon@gmail.com>
2024-08-21 17:43:31 -0400

    merged in upstream new Drupal versions w/ updated civi version

---
+++ build/drupal/10.3/php8.2/apache-bullseye/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chown www-data /var/www/drupal/web/sites/default
+/usr/local/bin/drupal-setup.sh
+# run the civi setup script if it exists!
+test -x /usr/local/bin/civicrm-setup.sh && $_
