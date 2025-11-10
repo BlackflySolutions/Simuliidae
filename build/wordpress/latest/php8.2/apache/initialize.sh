dea32d64aa29d07a895da50d7b0884fe0f928a4c
Alan Dixon <alan.g.dixon@gmail.com>
2025-11-10 15:30:21 -0500

    updated from wordpress reference project

--- build/wordpress/latest/php8.2/apache/initialize.sh
+++ build/wordpress/latest/php8.2/apache/initialize.sh
@@ -2,3 +2,8 @@
 # Set the mail and varnish host domains to the docker host ip for this container network.
 VHOST_DOCKER_IP=`/sbin/ip route | awk '/default/ { print $3 }'`
 echo "$VHOST_DOCKER_IP mail varnish" >> /etc/hosts
+# support for customizing your apache config, not recommended
+if [ -f "/var/www/html/conf/apache-custom.conf" ]; then
+  cp /var/www/html/conf/apache-custom.conf /etc/apache2/conf-available/vsite-custom.conf
+  a2enconf vsite-custom
+fi
