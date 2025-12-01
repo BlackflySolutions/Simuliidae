842f5ab444a1cb60bd9eac03cb3ec083bfe15f92
Alan Dixon <alan.g.dixon@gmail.com>
2025-12-01 17:19:22 -0500

    update docker-compose version to support custom network thing

--- compose/bin/docker-compose-install.sh
+++ compose/bin/docker-compose-install.sh
@@ -1,4 +1,4 @@
 # this project depends on the old docker-compose, pre-V2.
 # this script gets the latest pre-V2 version
-curl -SL https://github.com/docker/compose/releases/download/v1.25.2/docker-compose-linux-x86_64 -o docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-linux-x86_64 -o docker-compose
 chmod ugo+x docker-compose
