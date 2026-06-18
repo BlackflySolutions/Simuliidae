67595129d3d9c921c478efc9bad6d9642902c6eb
Alan Dixon <alan.g.dixon@gmail.com>
2026-06-18 14:33:50 +0000

    removed admin folder from 10.6 variants

--- build/drupal/10.6/php8.3/apache-bookworm/admin/backupsite.sh
+++
@@ -1,2 +0,0 @@
-# rsync sites dir to backup
-rsync --exclude files/js --exclude files/css --exclude templates_c -avz /var/www/html/sites/default/ /var/backup/volume/${VSITE}_vsite
--- build/drupal/10.6/php8.3/apache-bookworm/admin/root-ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ ! -z "$VSITE_SSH_USER" ]]; then
-  mkdir /root/.ssh/
-  cp /home/${VSITE_SSH_USER}/.ssh/hosted_id_rsa.pub /root/.ssh/authorized_keys
-  service ssh start
-fi
